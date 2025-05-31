03941b7a11e356df171c9051e277a3d676b894b4
sieluka <lukasz.sieradzkizsk@gmail.com>
2025-05-31 19:32:53 +0200

    Add login screen view

--- app/src/main/java/com/example/englishbee/screens/LoginPage.kt
+++ app/src/main/java/com/example/englishbee/screens/LoginPage.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
